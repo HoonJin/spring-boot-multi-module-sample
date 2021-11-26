f6b8a95063f331c6dfdd4b8f45cffd647a3202d6
Daniel Ji <bwjhj1030@gmail.com>
2021-11-26 20:55:49 +0900

    스프링부트 멀티모듈 프로젝트 세팅

---
+++ sample-domain/src/main/java/com/hoonjin/sample/domain/SampleDomainApplication.java
@@ -0,0 +1,12 @@
+package com.hoonjin.sample.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleDomainApplication.class);
+    }
+}
