a8d7860b4a1d50d4cf143c8d95d59d09f36e8995
Daniel Ji <bwjhj1030@gmail.com>
2021-12-11 01:12:21 +0900

    zuul 사용을 위한 2.4.x 다운그레이드 및 gateway 역할의 zuul applcation 추가

---
+++ sample-zuul/src/main/java/com/hoonjin/sample/zuul/filter/LoggingFilter.java
@@ -0,0 +1,29 @@
+package com.hoonjin.sample.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LoggingFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        log.info("asdfasdf");
+        return null;
+    }
+}
