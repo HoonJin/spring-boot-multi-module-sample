3422cd6208b8178e6029cee918edcf2a0b1e0632
Daniel Ji <bwjhj1030@gmail.com>
2021-12-11 19:20:42 +0900

    api gateway service 추가 및 build.gradle 수정

--- sample-first/src/main/java/com/hoonjin/sample/first/controller/FirstServiceController.java
+++ sample-first/src/main/java/com/hoonjin/sample/first/controller/FirstServiceController.java
@@ -1,13 +1,22 @@
 package com.hoonjin.sample.first.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class FirstServiceController {
 
-    @GetMapping("/welcome")
+    @GetMapping("/first-service/welcome")
     public String welcome() {
         return "welcome to the FIRST Service";
+    }
+
+    @GetMapping("/first-service/message")
+    public String message(@RequestHeader("First-Request-Id") String requestId) {
+        log.info("requestId = " + requestId);
+        return "HELLO to the FIRST Service";
     }
 }
--- sample-second/src/main/java/com/hoonjin/sample/second/controller/SecondServiceController.java
+++ sample-second/src/main/java/com/hoonjin/sample/second/controller/SecondServiceController.java
@@ -1,13 +1,22 @@
 package com.hoonjin.sample.second.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class SecondServiceController {
 
-    @GetMapping("/welcome")
+    @GetMapping("/second-service/welcome")
     public String welcome() {
         return "welcome to the SECOND Service";
+    }
+
+    @GetMapping("/second-service/message")
+    public String message(@RequestHeader("Second-Request-Id") String requestId) {
+        log.info("requestId = " + requestId);
+        return "HELLO to the SECOND Service";
     }
 }
