37966f099a84fbc4e9219a72441af2188181109f
Daniel Ji <bwjhj1030@gmail.com>
2021-12-12 17:53:09 +0900

    first, second service와 gateway를 유레카에 등록 및 first에 랜덤포트 적용

--- sample-first/src/main/java/com/hoonjin/sample/first/controller/FirstServiceController.java
+++ sample-first/src/main/java/com/hoonjin/sample/first/controller/FirstServiceController.java
@@ -1,22 +1,27 @@
 package com.hoonjin.sample.first.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class FirstServiceController {
+
+    private final Environment environment;
 
     @GetMapping("/first-service/welcome")
     public String welcome() {
-        return "welcome to the FIRST Service";
+        return "welcome to the FIRST Service: " + environment.getProperty("local.server.port");
     }
 
     @GetMapping("/first-service/message")
     public String message(@RequestHeader("First-Request-Id") String requestId) {
         log.info("requestId = " + requestId);
-        return "HELLO to the FIRST Service";
+        return "HELLO to the FIRST Service"  + environment.getProperty("local.server.port");
     }
 }
