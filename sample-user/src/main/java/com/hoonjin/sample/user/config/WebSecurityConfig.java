8e3e00c992eb1a97e1ee24f6c263890d27eec169
Daniel Ji <bwjhj1030@gmail.com>
2022-01-08 21:08:27 +0900

    config server 방식을 native로 변경하고 yml 파일 추가 & gateway, user service에서 config server에서 읽어오도록 cloud-start-config, bootstrap.yml 추가

--- sample-user/src/main/java/com/hoonjin/sample/user/config/WebSecurityConfig.java
+++ sample-user/src/main/java/com/hoonjin/sample/user/config/WebSecurityConfig.java
@@ -24,6 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
 //        http.authorizeRequests().antMatchers("/users/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
                 .hasIpAddress("0.0.0.0")
